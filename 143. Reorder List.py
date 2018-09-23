603b275406b285bc3e22036edb4ad970e7db1182
ahljljj <jliang0718@outlook.com>
2018-09-22 21:43:54 -0700

    modify 143

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -1,10 +1,6 @@
 """
 143. Reorder List
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 03447d33d86615a9944c8ef63e125326c791f5a9
 Given a singly linked list L: L0→L1→…→Ln-1→Ln,
 reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
 
@@ -20,11 +16,7 @@ Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
 
 """
 
-<<<<<<< HEAD
-=======
-'''
-wrong
->>>>>>> 03447d33d86615a9944c8ef63e125326c791f5a9
+
 
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -36,7 +28,6 @@ class Solution(object):
     def reorderList(self, head):
         """
         :type head: ListNode
-<<<<<<< HEAD
         :rtype: void Do not return anything, modify head in-place instead.
         """
         if head == None or head.next == None: return
@@ -75,32 +66,3 @@ class Solution(object):
             curr = tmp
         dummy.next = None
         return head
-
-
-
-=======
-        :rtype: 
-        void Do not return anything, modify head in-place instead.
-        """
-        self.helper(head)
-        
-    def pretail(self, head):
-        tmp = head
-        while tmp and tmp.next and tmp.next.next :
-            print('while loop', tmp.val)
-            tmp = tmp.next
-        return tmp
-    def helper(self, head):
-        if head == None or head.next == None: return
-        pretail = self.pretail(head)
-        tmp = head.next
-        tail = pretail.next
-        head.next = tail
-        pretail.next = None
-        tail.next = self.helper(tmp)
-        
-        
-        
-
-'''
->>>>>>> 03447d33d86615a9944c8ef63e125326c791f5a9
