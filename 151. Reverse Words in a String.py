4c3909bd7c037a1d9fbbb735ba03dbb1a42ee57a
Jinjin Liang <jliang0718@outlook.com>
2018-09-24 14:52:22 -0700

    another solution 151. Reverse Words in a String

--- 151. Reverse Words in a String.py
+++ 151. Reverse Words in a String.py
@@ -35,3 +35,31 @@ class Solution(object):
 
 
 '''
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        word = ""
+        words = ""
+        s = s[::-1]
+        for j, i in enumerate(s):
+            # character is not space, a current word exists,
+            # and previous character is space, e.g. i=b in " a b":
+            if i != " " and word != "" and s[j - 1] == " ":
+                # add current word to words and append " " to later add this i
+                words += (word + " ")
+                word = i
+            # character is not space, but it's either first character in string
+            # or is part of current word, e.g. i=b in "b", " b" "ab", "a ab "
+            elif i != " ":
+                word = i + word
+            else:
+                continue
+
+        words += word
+
+        return (words)
