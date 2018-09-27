dbcda47e495c2984b8458a6a9bdbd6c382acae31
Jinjin Liang <jliang0718@outlook.com>
2018-09-27 12:38:38 -0700

    166. Fraction to Recurring Decimal

--- 166. Fraction to Recurring Decimal.py
+++ 166. Fraction to Recurring Decimal.py
@@ -1,3 +1,27 @@
+"""
+166. Fraction to Recurring Decimal
+
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+Example 1:
+
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+Example 2:
+
+Input: numerator = 2, denominator = 1
+Output: "2"
+Example 3:
+
+Input: numerator = 2, denominator = 3
+Output: "0.(6)"
+
+
+"""
+
+
 class Solution(object):
     def fractionToDecimal(self, numerator, denominator):
         """
@@ -24,12 +48,9 @@ class Solution(object):
             if numerator > 0:
                 res += '.'
             else:
-                return res
+                return sign + res
         decimals[numerator] = len(res)
         numerator *= 10
-        #       while numerator < denominator:
-        #           numerator *= 10
-        #          res += '0'
         q = numerator
         d = denominator
         idx = len(res) + 1
@@ -43,11 +64,9 @@ class Solution(object):
                 else:
                     return sign + res[:decimals[r]] + '(' + res[decimals[r]:] + ')'
             else:
+                print(sign)
                 return sign + res
             q = r * 10
             idx += 1
 
 
-s = Solution()
-
-print(s.fractionToDecimal(-2147483648,1))
