bccb9f6e1c871b72059ca8a1319d49b17aea061a
ahljljj <jliang0718@outlook.com>
2018-09-30 22:24:31 -0700

    brute force to 201. Bitwise AND of Numbers Range

---
+++ 201. Bitwise AND of Numbers Range.py
@@ -0,0 +1,36 @@
+"""
+201. Bitwise AND of Numbers Range
+
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+
+Input: [5,7]
+Output: 4
+Example 2:
+
+Input: [0,1]
+Output: 0
+
+
+"""
+
+'''
+#bruteforce
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        res = m
+        for j in xrange(m+1, n+1):
+            if res == 0:
+                return res
+            res = res & j
+        return res
+        
+
+'''
