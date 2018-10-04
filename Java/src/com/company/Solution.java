d1f674b733e806c9e3151fe73db5c45e1700927c
Jinjin Liang <jliang0718@outlook.com>
2018-10-04 15:11:00 -0700

    python and java to 220. Contains Duplicate III

---
+++ Java/src/com/company/Solution.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import com.sun.source.util.Trees;
+
+import java.util.TreeSet;
+
+public class Solution {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+        if (nums == null || nums.length == 0 || k <=0 || t < 0){
+            return false;
+        }
+
+        TreeSet<Integer> set = new TreeSet();
+        for (int i = 0; i < nums.length; i++){
+            // the smallest number larger than nums[i]
+            Integer ceil = set.ceiling(nums[i]);
+            if (ceil != null && Long.valueOf(ceil) - Long.valueOf(nums[i]) <= t){
+                return true;
+            }
+            // the larget number smaller than nums[i]
+            Integer floor = set.floor(nums[i]);
+            if (floor != null && Long.valueOf(nums[i]) - Long.valueOf(floor) <=t){
+                return true;
+            }
+            set.add(nums[i]);
+            if (i >= k){
+                set.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+
+}
