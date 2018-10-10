bd937a36b5f4c79563c1c89f15a2e4a12a14e336
ahljljj <jliang0718@outlook.com>
2018-10-09 23:18:03 -0700

    simple way to 237. Delete Node in a Linked List

--- 237. Delete Node in a Linked List.py
+++ 237. Delete Node in a Linked List.py
@@ -47,3 +47,25 @@ class Solution:
             node.val = tmp.val
             node = tmp
         curr.next = None
+
+
+
+'''
+# Since we do not have access to the node before the one we want to delete, we cannot modify the next pointer of that node in any way. Instead, we have to replace the value of the node we want to delete with the value in the node after it, and then delete the node after it.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+'''
