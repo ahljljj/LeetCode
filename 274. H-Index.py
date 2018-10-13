f409bcbe67d69ea9e6481f58130de4c70e53fa4a
ahljljj <jliang0718@outlook.com>
2018-10-13 11:44:21 -0700

    two other solutions to 274

--- 274. H-Index.py
+++ 274. H-Index.py
@@ -53,3 +53,52 @@ class Solution:
             else:
                 hidx = tmp
         return hidx
+
+
+'''
+#O(nlog(n))
+
+class Solution:
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort()
+        n = len(citations)
+        for i in range(n):
+            if citations[i] >= (n-i):
+                return n-i
+        return 0
+
+
+'''
+
+
+'''
+
+#O(n) time and space
+
+
+class Solution:
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        citeCount = [0] * (n+1)
+        for c in citations:
+            if c >= n:
+                citeCount[n] += 1
+            else:
+                citeCount[c] += 1
+
+        i = n-1
+        while i >= 0:
+            citeCount[i] += citeCount[i+1]
+            if citeCount[i+1] >= i+1:
+                return i+1
+            i -= 1
+        return 0
+'''
