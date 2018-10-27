d152d9a84190840adcbd19aa1cfc216542c97312
ahljljj <jliang0718@outlook.com>
2018-10-26 22:11:59 -0700

    two pointers to 349

---
+++ 349. Intersection of Two Arrays.py
@@ -0,0 +1,66 @@
+"""
+349. Intersection of Two Arrays
+
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+
+
+"""
+
+#hashmap O(n + m)
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        res = []
+        for num in nums1:
+            if num in nums2:
+                res.append(num)
+        return res
+
+
+# two pointers
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        nums1.sort()
+        nums2.sort()
+        i = j = 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                if nums1[i] not in res:
+                    res.append(nums1[i])
+                i += 1
+                j += 1
+
+        return res
