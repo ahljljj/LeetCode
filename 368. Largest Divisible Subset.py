43e4cf0a48daab4d849459f324d5a8fb2cd23e41
ahljljj <jliang0718@outlook.com>
2018-10-29 23:48:22 -0700

    dp to 368. Largest Divisible Subset

--- 368. Largest Divisible Subset.py
+++ 368. Largest Divisible Subset.py
@@ -100,3 +100,53 @@ class Solution:
         for key in hashmap:
             if len(hashmap[key]) == res:
                 return hashmap[key]
+
+
+# dynamic programming
+
+
+'''
+idea
+
+The basic idea is like:
+
+1. Sort
+2. Find the length of longest subset
+3. Record the largest element of it.
+4. Do a loop from the largest element to nums[0], add every element belongs to the longest subset.
+
+'''
+
+
+class Solution:
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        nums.sort()
+        n = len(nums)
+        dp = [1] * n  # dp[i]: length of the maximal possible subset at nums[i]
+        for i in range(1, n):
+            for j in range(0, i):
+                if nums[i] % nums[j] == 0:
+                    dp[i] = max(dp[i], 1 + dp[j])
+        # find the max index: the maximal element in the longest subset
+        maxidx = 0
+        for i in range(n):
+            maxidx = i if dp[i] > dp[maxidx] else maxidx
+        # going backwards, use the max index to find the corresponding subset
+        tmp = nums[maxidx]
+        res = []
+        curdp = dp[maxidx]
+        for i in range(maxidx, -1, -1):
+            if tmp % nums[i] == 0 and dp[i] == curdp:
+                res = [nums[i]] + res
+                tmp = nums[i]
+                curdp -= 1
+        return res
+
+
+
