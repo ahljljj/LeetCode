8999bfb50744e58547664258d831223d53d96e94
Jinjin Liang <jliang0718@outlook.com>
2018-10-30 10:48:49 -0700

    math 372. Super Pow

---
+++ 372. Super Pow.py
@@ -0,0 +1,32 @@
+"""
+372. Super Pow
+
+
+Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+
+Example 1:
+
+Input: a = 2, b = [3]
+Output: 8
+Example 2:
+
+Input: a = 2, b = [1,0]
+Output: 1024
+
+
+
+"""
+
+
+class Solution:
+    def superPow(self, a, b):
+        """
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        """
+        res = 1
+        for digit in b:
+            tmp = (a ** digit) % 1337
+            res = ((res ** 10) * tmp) % 1337
+        return res
