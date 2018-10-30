0c6b7642a5878ab6a5c2c273201e91c351edc08d
ahljljj <jliang0718@outlook.com>
2018-10-29 20:09:56 -0700

    newton's method to 367

--- 367. Valid Perfect Square.py
+++ 367. Valid Perfect Square.py
@@ -16,7 +16,7 @@ Input: 14
 Output: false
 """
 
-# binary search
+# binary search O(lgn)
 
 class Solution(object):
     def isPerfectSquare(self, num):
@@ -36,3 +36,43 @@ class Solution(object):
             else:
                 left = mid + 1
         return False
+
+# math: time complexity O(root(n))
+# idea: 1 + 3 + ... + (2k - 1) = k^2
+# every perfect square can be written in the above form
+
+class Solution:
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        i = 1
+        res = i
+        while i:
+            if res == num:
+                return True
+            elif res > num:
+                return False
+            i += 2
+
+
+            res += i
+
+
+# newton's method time complexity: O(0.5ln(n)) faster than binary search
+# f(x) = f'(xn)(x - xn) + f(xn)  ----> = 0
+# x(n+1) = x(n) - f(xn)/f'(xn) = 0.5(x + n/x)
+# n/2^k > n^0.5 ---> 2^k < n^0.5---> k < 0.5lg(n)
+
+
+class Solution:
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        x = num
+        while (x **2 > num):
+            x = (x + num // x)>>1
+        return num == x**2
