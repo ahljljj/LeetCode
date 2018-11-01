235eb9963dc9c1c93b586f31f102a5fcedb1fadb
ahljljj <jliang0718@outlook.com>
2018-10-31 23:37:59 -0700

    divide and conquer

--- C++/LeetCode/53. Maximum Subarray.cpp
+++ C++/LeetCode/53. Maximum Subarray.cpp
@@ -74,3 +74,70 @@ public:
         return res;   
     } 
 };
+
+// divide and conquer
+/*
+ * 1) Divide the given array in two halves
+2) Return the maximum of following three
+….a) Maximum subarray sum in left half (Make a recursive call)
+….b) Maximum subarray sum in right half (Make a recursive call)
+….c) Maximum subarray sum such that the subarray crosses the midpoint
+
+The lines 2.a and 2.b are simple recursive calls. 
+ * How to find maximum subarray sum such that the subarray crosses the midpoint? 
+ * We can easily find the crossing sum in linear time. 
+ * The idea is simple, find the maximum sum starting from mid point and ending at some point on left of mid, 
+ * then find the maximum sum starting from mid + 1 and ending with sum point on right of mid + 1. 
+ * Finally, combine the two and return.
+ * */
+
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    
+        
+        return maxSub(nums, 0, nums.size() - 1);
+        
+    }
+    
+    int maxSub(vector<int>& nums, int left, int right){
+        // return if there is only one element
+        if (left == right){
+            return nums[left];
+        }
+        
+        // find the middle point
+        int mid = (left + right)/2;
+        
+        /* Return maximum of following three possible cases 
+      a) Maximum subarray sum in left half 
+      b) Maximum subarray sum in right half 
+      c) Maximum subarray sum such that the subarray crosses the midpoint */
+        
+        return max({maxSub(nums, left, mid), 
+                    maxSub(nums, mid + 1, right),
+                   maxCrossmid(nums, left, mid, right)});        
+    }
+    
+    int maxCrossmid(vector<int> & nums, int left, int mid, int right){
+        int left_sum = INT_MIN, right_sum = INT_MIN, sum{0};
+        // scan element from left of mid
+        for (int i = mid; i >= left; i--){
+            sum += nums[i];
+            left_sum = max(sum, left_sum);
+        }
+        sum = 0;
+        // scan element from right of mid
+        for (int i = mid + 1; i <= right; i++){
+            sum += nums[i];
+            right_sum = max(sum, right_sum);            
+        }
+        // return the summation
+        return left_sum + right_sum;        
+    }
+    
+    
+    
+};
