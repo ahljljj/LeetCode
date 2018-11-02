f155239fc78d843811763cfd253a4c9b0d5b38a1
ahljljj <jliang0718@outlook.com>
2018-11-01 20:32:58 -0700

    dictionary + dictionary

--- 380. Insert Delete GetRandom O(1).py
+++ 380. Insert Delete GetRandom O(1).py
@@ -89,3 +89,58 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+
+
+# dictionary + dictionary
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.res = {}
+        self.nums = {}
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.res:
+            idx = len(self.res)
+            self.nums[idx] = val
+            self.res[val] = idx
+            return True
+        return False
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.res:
+            idx = self.res[val]
+            last = self.nums[len(self.nums) - 1]
+            self.nums[idx] = last
+            self.res[last] = idx
+            self.res.pop(val, None)
+            self.nums.pop(len(self.nums) - 1, None)
+            return True
+        return False
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
