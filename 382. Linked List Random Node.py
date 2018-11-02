926fc1609af15e7080a2e75d5af14cc50fa51766
Jinjin Liang <jliang0718@outlook.com>
2018-11-02 12:32:43 -0700

    reservoir sampling

--- 382. Linked List Random Node.py
+++ 382. Linked List Random Node.py
@@ -61,3 +61,97 @@ class Solution:
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(head)
 # param_1 = obj.getRandom()
+
+
+
+
+# reservoir sampling
+
+'''
+intuition
+
+Problem:
+Choose k entries from n numbers. Make sure each number is selected with the probability of k/n
+Basic idea:
+Choose 1, 2, 3, ..., k first and put them into the reservoir.
+For k+1, pick it with a probability of k/(k+1), and randomly replace a number in the reservoir.
+For k+i, pick it with a probability of k/(k+i), and randomly replace a number in the reservoir.
+Repeat until k+i reaches n
+Proof:
+For k+i, the probability that it is selected and will replace a number in the reservoir is k/(k+i)
+For a number in the reservoir before (let's say X), the probability that it keeps staying in the reservoir is
+P(X was in the reservoir last time) × P(X is not replaced by k+i)
+= P(X was in the reservoir last time) × (1 - P(k+i is selected and replaces X))
+= k/(k+i-1) × （1 - k/(k+i) × 1/k）
+= k/(k+i)
+When k+i reaches n, the probability of each number staying in the reservoir is k/n
+Example
+Choose 3 numbers from [111, 222, 333, 444]. Make sure each number is selected with a probability of 3/4
+First, choose [111, 222, 333] as the initial reservior
+Then choose 444 with a probability of 3/4
+For 111, it stays with a probability of
+P(444 is not selected) + P(444 is selected but it replaces 222 or 333)
+= 1/4 + 3/4*2/3
+= 3/4
+The same case with 222 and 333
+Now all the numbers have the probability of 3/4 to be picked
+This Problem
+This problem is the sp case where k=1
+
+
+
+
+Start...
+When we read the first node head, if the stream ListNode stops here, we can just return the head.val. The possibility is 1/1.
+
+When we read the second node, we can decide if we replace the result r or not. The possibility is 1/2. So we just generate a random number between 0 and 1, and check if it is equal to 1. If it is 1, replace r as the value of the current node, otherwise we don't touch r, so its value is still the value of head.
+
+When we read the third node, now the result r is one of value in the head or second node. We just decide if we replace the value of r as the value of current node(third node). The possibility of replacing it is 1/3, namely the possibility of we don't touch r is 2/3. So we just generate a random number between 0 ~ 2, and if the result is 2 we replace r.
+
+We can continue to do like this until the end of stream ListNode.
+
+
+'''
+
+
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+
+    def __init__(self, head):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+        self.head = head
+
+    def getRandom(self):
+        """
+        Returns a random node's value.
+        :rtype: int
+        """
+        dummy = self.head
+        res = None
+        count = 0
+        idx = 0
+        while dummy:
+            count += 1
+            idx = random.randint(1, count)
+            if idx == count:
+                res = dummy.val
+            dummy = dummy.next
+        return res
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
