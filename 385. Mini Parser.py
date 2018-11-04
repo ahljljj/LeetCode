2faeab39411186dc3761eb40fd9dd43f7de14e48
ahljljj <jliang0718@outlook.com>
2018-11-03 17:57:32 -0700

    reference and iteration

--- 385. Mini Parser.py
+++ 385. Mini Parser.py
@@ -265,6 +265,79 @@ class Solution:
         return res
 
 
+# reference / fast iteration, similar to the idea of recursion
+# time complexity O(n)
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def deserialize(self, s):
+        """
+        :type s: str
+        :rtype: NestedInteger
+        """
+        nums = ""
+        stack = []
+        last = None
+        for ch in s:
+            if ch == "[":
+                elem = NestedInteger()
+                if stack: stack[-1].add(elem)
+                stack.append(elem) # append the physical address of this element to the end of the stack
+            elif ch == ",":
+                if nums:
+                    stack[-1].add(NestedInteger(int(nums)))
+                    nums = ""
+            elif ch == "]":
+                if nums:
+                    stack[-1].add(NestedInteger(int(nums)))
+                    nums = ""
+                last = stack.pop()
+            else:
+                nums += ch
+        return last if last else NestedInteger(int(nums))
+
 
 
 
