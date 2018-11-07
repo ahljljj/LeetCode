1045e1fb00d031b74dbcf113167e1508cbbde5c4
ahljljj <jliang0718@outlook.com>
2018-11-06 22:47:31 -0700

    idea to binary search

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -52,3 +52,21 @@ class Solution:
                 return False
             isin = False
         return True
+
+# binary search
+'''
+intuition
+
+    // Follow-up: O(N) time for pre-processing, O(Mlog?) for each S.
+    // Eg-1. s="abc", t="bahbgdca"
+    // idx=[a={1,7}, b={0,3}, c={6}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=3
+    //  i=2 ('c'): prev=6 (return true)
+    // Eg-2. s="abc", t="bahgdcb"
+    // idx=[a={1}, b={0,6}, c={5}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=6
+    //  i=2 ('c'): prev=? (return false)
+
+'''
