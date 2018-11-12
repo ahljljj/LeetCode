86e9e4c3849c6d7816f4f428e50db5b98bb3dd9a
ahljljj <jliang0718@outlook.com>
2018-11-11 20:08:21 -0700

    one line bit manipulation

--- 401. Binary Watch.py
+++ 401. Binary Watch.py
@@ -104,6 +104,21 @@ class Solution:
                 m -= self.minutes[i - len(self.hours)]
 
 
+# one line bit manipulation
+
+class Solution:
+    def readBinaryWatch(self, num):
+        """
+        :type num: int
+        :rtype: List[str]
+        """
+
+        return [str(h) + (":0" if m < 10 else ":") + str(m)
+                for h in range(12) for m in range(60)
+                if bin(h).count("1") + bin(m).count("1") == num]
+
+
+
 
 
 
