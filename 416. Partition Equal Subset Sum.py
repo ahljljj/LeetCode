22d807c4fd93961f61480693d457e97a97d52776
Jinjin Liang <jliang0718@outlook.com>
2018-11-14 14:16:34 -0700

    dfs (tle)

---
+++ 416. Partition Equal Subset Sum.py
@@ -0,0 +1,52 @@
+"""
+416. Partition Equal Subset Sum
+
+
+Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Note:
+Each of the array element will not exceed 100.
+The array size will not exceed 200.
+Example 1:
+
+Input: [1, 5, 11, 5]
+
+Output: true
+
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: [1, 2, 3, 5]
+
+Output: false
+
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+
+"""
+
+# brute force (tle): try all possible situations
+
+
+class Solution:
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        mean = total / 2
+
+        return self.helper(nums, mean, 0)
+
+    def helper(self, nums, target, idx):
+        if target == 0:
+            return True
+        for i in range(idx, len(nums)):
+            if target >= nums[i] and self.helper(nums, target - nums[i], i + 1):
+                return True
+        return False
+
+
