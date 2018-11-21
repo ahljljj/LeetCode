56b73346f238e724fbe705eeeb5e18621cd6c421
ahljljj <jliang0718@outlook.com>
2018-11-21 00:03:02 -0700

    recursion using two functions

--- 437. Path Sum III.py
+++ 437. Path Sum III.py
@@ -96,3 +96,45 @@ class Solution:
                 self.dfs(root.right, sum, root.right.val)
             self.used.add(root.right)
         return
+
+
+# recursion using two functions
+# better than my idea (more clear and clean)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        if not root:
+            return 0
+        self.count = 0
+        self.helper(root, sum)
+        return self.count
+
+    def dfs(self, root, sum):
+        if not root:
+            return
+        if root.val == sum:
+            self.count += 1
+        if root.left:
+            self.dfs(root.left, sum - root.val)
+        if root.right:
+            self.dfs(root.right, sum - root.val)
+
+    def helper(self, root, sum):
+        if not root:
+            return
+        self.dfs(root, sum)
+        if root.left: self.helper(root.left, sum)
+        if root.right: self.helper(root.right, sum)
+
