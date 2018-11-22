9a9bcb933e180a636a4b13130d9a32acd8e0da76
ahljljj <jliang0718@outlook.com>
2018-11-21 19:22:29 -0700

    swap / change the original array

---
+++ 442. Find All Duplicates in an Array.py
@@ -0,0 +1,42 @@
+"""
+442. Find All Duplicates in an Array
+
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+"""
+
+# swap / change the original array
+# maintain the visited nodes
+
+
+class Solution:
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        i = 0
+        res = []
+        while i < len(nums):
+            idx = nums[i] - 1
+            if idx == -2 or idx == i:
+                i += 1
+                continue
+            if idx != i:
+                if nums[idx] != nums[i]:
+                    nums[i], nums[idx] = nums[idx], nums[i]
+                else:
+                    res.append(nums[i])
+                    nums[i] = -1
+        return res
+
