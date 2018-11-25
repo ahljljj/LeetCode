862422af23c4833c939fef76dbeb7b1a0053082c
ahljljj <jliang0718@outlook.com>
2018-11-24 23:55:11 -0700

    iterative

--- 450. Delete Node in a BST.py
+++ 450. Delete Node in a BST.py
@@ -92,4 +92,72 @@ class Solution:
         return root
 
 
+# iterative
+
+'''
+Find the node to be removed and its parent using binary search, and then use deleteRootNode to delete the root node of the subtree and return the new root node. This idea is taken from https://discuss.leetcode.com/topic/67309/an-easy-understanding-o-h-time-o-1-space-java-solution.
+
+I'd also like to share my thinkings of the other solutions I've seen.
+
+There are many solutions that got high votes using recursive approach, including the ones from the Princeton's Algorithm and Data Structure book. Don't you notice that recursive approach always takes extra space? Why not consider the iterative approach first?
+Some solutions swap the values instead of swapping the nodes. In reality, the value of a node could be more complicated than just a single integer, so copying the contents might take much more time than just copying the reference.
+As for the case when both children of the node to be deleted are not null, I transplant the successor to replace the node to be deleted, which is a bit harder to implement than just transplant the left subtree of the node to the left child of its successor. The former way is used in many text books too. Why? My guess is that transplanting the successor can keep the height of the tree almost unchanged, while transplanting the whole left subtree could increase the height and thus making the tree more unbalanced.
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        curr = root  # target, the node with the key and which should be deleted
+        prev = None
+        while curr and curr.val != key:
+            prev = curr
+            if key < curr.val:
+                curr = curr.left
+            else:
+                curr = curr.right
+        if not prev:
+            return self.deleteRootNode(curr)
+        if prev.left == curr:
+            prev.left = self.deleteRootNode(curr)
+        else:
+            prev.right = self.deleteRootNode(curr)
+        return root
+
+    def deleteRootNode(self, root):
+        # delete the target node and return the new root node
+        if not root:
+            return root
+        if not root.left:
+            return root.right
+        if not root.right:
+            return root.left
+            # minKid: the minimum node of the right subtree
+        minKid = root.right
+        # we need swap root and minKid
+        # minKid is the new root
+        while minKid and minKid.left:
+            parent = minKid
+            minKid = minKid.left
+        minKid.left = root.left
+        if minKid != root.right:
+            parent.left = minKid.right
+            minKid.right = root.right
+        return minKid
+
+
+
+
+
 
