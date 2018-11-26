d299b4ac86f1689ec09cbe6d05eb7c8d29ba3b42
Jinjin Liang <jliang0718@outlook.com>
2018-11-26 00:30:12 -0700

    hashtable, not good on time complexity

--- 18. 4Sum.py
+++ 18. 4Sum.py
@@ -52,3 +52,35 @@ class Solution(object):
                         r -= 1
         return res
 
+# hashtable
+# time complexity O(n^2 ~ n^3)
+
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        getSum = {}
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                tmp = nums[i] + nums[j]
+                if tmp not in getSum:
+                    getSum[tmp] = set([(i, j)])
+                else:
+                    getSum[tmp].add((i, j))
+        res = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                r = target - nums[i] - nums[j]
+                if r in getSum:
+                    for (idx1, idx2) in getSum[r]:
+                        if j < idx1 and [nums[i], nums[j], nums[idx1], nums[idx2]] not in res:
+                            res.append([nums[i], nums[j], nums[idx1], nums[idx2]])
+        return res
+
+
+
+
