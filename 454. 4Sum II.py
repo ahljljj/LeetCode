7ebbdc09e1ca27bd6a0feab7c70fa490979814ae
Jinjin Liang <jliang0718@outlook.com>
2018-11-26 16:42:52 -0700

    TLE and barely AC binary search

--- 454. 4Sum II.py
+++ 454. 4Sum II.py
@@ -59,3 +59,180 @@ class Solution:
                     res += sumAB[-tmp]
         return res
 
+# binary search: TLE
+
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        sumCD = []
+        for nC in C:
+            for nD in D:
+                sumCD.append(nC + nD)
+        sumCD.sort()
+        res = 0
+        for nA in A:
+            for nB in B:
+                target = -(nA + nB)
+                right = self.upper(sumCD, target)
+                if sumCD[right] != target:
+                    continue
+                left = self.lower(sumCD, target)
+                res += right - left + 1
+        return res
+
+
+def upper(self, nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) >> 1
+        if nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left - 1
+
+
+def lower(self, nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) >> 1
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return right + 1
+
+# binary search: TLE
+
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        sumCD = []
+        for nC in C:
+            for nD in D:
+                sumCD.append(nC + nD)
+        sumCD.sort()
+        res = 0
+        for nA in A:
+            for nB in B:
+                target = -(nA + nB)
+                exist = self.biSearch(sumCD, target)
+                if exist == -1:
+                    continue
+                right = self.upper(sumCD, exist, target)
+                left = self.lower(sumCD, exist, target)
+                res += right - left + 1
+        return res
+
+    def biSearch(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+    def upper(self, nums, lower, target):
+        left, right = lower, len(nums) - 1
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left - 1
+
+    def lower(self, nums, upper, target):
+        left, right = 0, upper
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right + 1
+
+
+# barely accepted binary search
+# time complexity O(n^2lgn)
+
+'''
+first add x (foreach in vector A) and y (foreach in vector B) ,put the sum into vector s,do the same to vector C,D and new vector called m; for every x in new vector s, we find how many -x in vector m, use binary search to do this ,This is the binary search code
+
+'''
+
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        sumCD = []
+        sumAB = []
+        for nC in C:
+            for nD in D:
+                sumCD.append(nC + nD)
+        for nA in A:
+            for nB in B:
+                sumAB.append(nA + nB)
+        sumCD.sort()
+        sumAB.sort()
+        res, tmp = 0, 0
+        for i in range(len(sumAB)):
+            target = - sumAB[i]
+            if i > 0 and sumAB[i] == sumAB[i - 1]:
+                res += tmp
+            else:
+                right = self.upper(sumCD, target)
+                if sumCD[right] != target:
+                    tmp = 0
+                    continue
+                left = self.lower(sumCD, target)
+                tmp = right - left + 1
+                res += tmp
+        return res
+
+
+    def upper(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
+
+
+    def lower(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+
+
+
