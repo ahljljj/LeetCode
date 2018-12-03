a39f20f1de0b5ebbc1e2cca5d22fa7c3d16e070f
ahljljj <jliang0718@outlook.com>
2018-12-03 00:17:47 -0700

    list compression, pre-processing

---
+++ 244. Shortest Word Distance II.py
@@ -0,0 +1,48 @@
+"""
+244. Shortest Word Distance II
+
+
+Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list. Your method will be called repeatedly many times with different parameters.
+
+Example:
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Input: word1 = “coding”, word2 = “practice”
+Output: 3
+Input: word1 = "makes", word2 = "coding"
+Output: 1
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+
+
+"""
+
+# list compression, pre-processing
+
+
+class WordDistance:
+
+    def __init__(self, words):
+        """
+        :type words: List[str]
+        """
+        self.map = {}
+        for i, word in enumerate(words):
+            if word not in self.map:
+                self.map[word] = [i]
+            else:
+                self.map[word].append(i)
+
+    def shortest(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        l1 = self.map[word1]
+        l2 = self.map[word2]
+        return min([abs(x - y) for x in l1 for y in l2])
+
+# Your WordDistance object will be instantiated and called as such:
+# obj = WordDistance(words)
+# param_1 = obj.shortest(word1,word2)
