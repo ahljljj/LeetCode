f1e2cff0ef322fe87492b5449e159b3f5bb4252f
Jinjin Liang <jliang0718@outlook.com>
2018-12-06 13:15:45 -0700

    brilliant

--- 247. Strobogrammatic Number II.py
+++ 247. Strobogrammatic Number II.py
@@ -50,4 +50,47 @@ class Solution:
             self.dfs(res, tmp + s[i], n)
         return
 
+# brilliant
+
+'''
+
+Some observation to the sequence:
+
+n == 1: [0, 1, 8]
+
+n == 2: [11, 88, 69, 96]
+
+How about n == 3?
+=> it can be retrieved if you insert [0, 1, 8] to the middle of solution of n == 2
+
+n == 4?
+=> it can be retrieved if you insert [11, 88, 69, 96, 00] to the middle of solution of n == 2
+
+n == 5?
+=> it can be retrieved if you insert [0, 1, 8] to the middle of solution of n == 4
+
+the same, for n == 6, it can be retrieved if you insert [11, 88, 69, 96, 00] to the middle of solution of n == 4
+
+'''
+
+class Solution:
+    def findStrobogrammatic(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        evenMid = ["00", "11", "69", "88", "96"]
+        oddMid = ["0", "1", "8"]
+        if n == 1:
+            return oddMid
+        if n == 2:
+            return evenMid[1:]
+        if n % 2 == 0:
+            prev, mid = self.findStrobogrammatic(n - 2), evenMid
+        else:
+            prev, mid = self.findStrobogrammatic(n - 1), oddMid
+        idx = (n - 1) >> 1
+        return [p[:idx] + c + p[idx:] for c in mid for p in prev]
+
+
 
