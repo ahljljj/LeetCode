c03a61ac41f0535478fbdbc543154f23ed93c088
ahljljj <jliang0718@outlook.com>
2018-12-07 09:16:55 -0700

    python stack cheating

---
+++ 251. Flatten 2D Vector.py
@@ -0,0 +1,57 @@
+"""
+251. Flatten 2D Vector
+
+
+Implement an iterator to flatten a 2d vector.
+
+Example:
+
+Input: 2d vector =
+[
+  [1,2],
+  [3],
+  [4,5,6]
+]
+Output: [1,2,3,4,5,6]
+Explanation: By calling next repeatedly until hasNext returns false,
+             the order of elements returned by next should be: [1,2,3,4,5,6].
+Follow up:
+As an added challenge, try to code it using only iterators in C++ or iterators in Java.
+
+
+"""
+
+# python stack cheating
+
+class Vector2D(object):
+
+    def __init__(self, vec2d):
+        """
+        Initialize your data structure here.
+        :type vec2d: List[List[int]]
+        """
+        self.vec = vec2d[::-1]
+        self.stack = []
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if self.stack:
+            return True
+        elif self.vec:
+            self.stack = self.vec.pop()[::-1]
+            while self.vec and not self.stack:
+                self.stack = self.vec.pop()[::-1]
+            return True if self.stack else False
+        return False
+
+# Your Vector2D object will be instantiated and called as such:
+# i, v = Vector2D(vec2d), []
+# while i.hasNext(): v.append(i.next())
