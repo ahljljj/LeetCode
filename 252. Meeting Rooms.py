8ba0d215bc6de71e0d85f39ce14d9bce175690e3
ahljljj <jliang0718@outlook.com>
2018-12-07 11:47:19 -0700

    c++ sort

--- 252. Meeting Rooms.py
+++ 252. Meeting Rooms.py
@@ -35,3 +35,33 @@ class Solution:
             if intervals[i].start < intervals[i - 1].end:
                 return False
         return True
+
+
+# c++
+'''
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    static bool cmp(Interval i1, Interval i2){
+        return i1.start < i2.start;
+    }
+    
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp);
+        for(int i = 1; i < intervals.size(); i ++)
+            if (intervals[i].start < intervals[i - 1].end)
+                return false;
+        return true;
+    }
+};
+
+'''
