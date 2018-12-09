77a982c125a8d87818014f0f12bdd0058f48411f
ahljljj <jliang0718@outlook.com>
2018-12-08 18:10:22 -0700

    py dp

---
+++ 256. Paint House.py
@@ -0,0 +1,42 @@
+"""
+256. Paint House
+
+
+There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+
+Example:
+
+Input: [[17,2,17],[16,16,5],[14,3,19]]
+Output: 10
+Explanation: Paint house 0 into blue, paint house 1 into green, paint house 2 into blue.
+             Minimum cost: 2 + 5 + 3 = 10.
+
+
+
+"""
+
+# dynamic programming
+
+class Solution:
+    def minCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        dpr, dpb, dpg = [0] * len(costs), [0] * len(costs), [0] * len(costs)
+        res = float('inf')
+        for i in range(len(costs)):
+            if i == 0:
+                dpr[i], dpb[i], dpg[i] = costs[0][0], costs[0][1], costs[0][2]
+                res = min([dpr[i], dpb[i], dpg[i]])
+                continue
+            dpr[i] = costs[i][0] + min(dpb[i - 1], dpg[i - 1])
+            dpb[i] = costs[i][1] + min(dpr[i - 1], dpg[i - 1])
+            dpg[i] = costs[i][2] + min(dpb[i - 1], dpr[i - 1])
+            res = min([dpr[i], dpb[i], dpg[i]])
+        return res if res < float('inf') else 0
