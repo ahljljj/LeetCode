53bf20e421126d03e9808e2b473562da4bb92965
ahljljj <jliang0718@outlook.com>
2018-12-10 11:18:40 -0700

    hashtable

---
+++ 266. Palindrome Permutation.py
@@ -0,0 +1,45 @@
+"""
+266. Palindrome Permutation
+
+
+Given a string, determine if a permutation of the string could form a palindrome.
+
+Example 1:
+
+Input: "code"
+Output: false
+Example 2:
+
+Input: "aab"
+Output: true
+Example 3:
+
+Input: "carerac"
+Output: true
+
+
+
+"""
+
+
+# hashtable
+
+class Solution:
+    def canPermutePalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        hMap = {}
+        for ch in s:
+            if ch not in hMap:
+                hMap[ch] = 1
+            else:
+                hMap[ch] += 1
+        count = 0
+        for (ch, freq) in hMap.items():
+            if freq % 2:
+                count += 1
+            if count > 1:
+                return False
+        return True
