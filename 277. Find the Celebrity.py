ede83a20949a0a845e29547174886b06f7f6c129
ahljljj <jliang0718@outlook.com>
2018-12-12 16:26:26 -0700

    brilliant

--- 277. Find the Celebrity.py
+++ 277. Find the Celebrity.py
@@ -41,3 +41,41 @@ class Solution(object):
                 return a
             valid = True
         return -1
+
+# brilliant
+
+'''
+Explanation
+
+The first loop is to exclude n - 1 labels that are not possible to be a celebrity.
+After the first loop, x is the only candidate.
+The second and third loop is to verify x is actually a celebrity by definition.
+
+The key part is the first loop. To understand this you can think the knows(a,b) as a a < b comparison, if a knows b then a < b, if a does not know b, a > b. Then if there is a celebrity, he/she must be the "maximum" of the n people.
+
+However, the "maximum" may not be the celebrity in the case of no celebrity at all. Thus we need the second and third loop to check if x is actually celebrity by definition.
+
+The total calls of knows is thus 3n at most. One small improvement is that in the second loop we only need to check i in the range [0, x). You can figure that out yourself easily.
+
+'''
+
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        max = 0
+        for i in range(n):
+            if knows(max, i): max = i
+        for i in range(max):
+            if knows(max, i): return -1
+        for i in range(n):
+            if not knows(i, max): return -1
+        return max
