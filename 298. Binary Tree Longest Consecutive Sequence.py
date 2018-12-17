feb1644cf5a480034aa1dc61ea694df73975d3d5
ahljljj <jliang0718@outlook.com>
2018-12-17 11:33:10 -0700

    python, standard dfs

---
+++ 298. Binary Tree Longest Consecutive Sequence.py
@@ -0,0 +1,74 @@
+'''
+
+298. Binary Tree Longest Consecutive Sequence
+
+Given a binary tree, find the length of the longest consecutive sequence path.
+
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+
+Example 1:
+
+Input:
+
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+
+Output: 3
+
+Explanation: Longest consecutive sequence path is 3-4-5, so return 3.
+Example 2:
+
+Input:
+
+   2
+    \
+     3
+    /
+   2
+  /
+ 1
+
+Output: 2
+
+Explanation: Longest consecutive sequence path is 2-3, not 3-2-1, so return 2.
+
+'''
+
+# python, standard dfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def longestConsecutive(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        self.res = 0
+        self.dfs(root, None, 1)
+        return self.res
+
+    def dfs(self, root, prev, tmp):
+        if not root:
+            self.res = max(self.res, tmp)
+            return
+        if prev != None:
+            if root.val == prev + 1:
+                tmp += 1
+            else:
+                tmp = 1
+            self.res = max(self.res, tmp)
+        self.dfs(root.left, root.val, tmp)
+        self.dfs(root.right, root.val, tmp)
+
