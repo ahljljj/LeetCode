84510a0f38e75cf5ca4af7f81fca7bd05bf851b9
ahljljj <jliang0718@outlook.com>
2018-12-23 14:40:02 -0700

    better cpp

--- 543. Diameter of Binary Tree.cpp
+++ 543. Diameter of Binary Tree.cpp
@@ -48,3 +48,31 @@ public:
     }
 
 };
+
+// better cpp
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int res = 0;
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        depth(root);
+        return res;
+
+    }
+    int depth(TreeNode * root){
+        if (!root) return 0;
+        int left = depth(root->left);
+        int right = depth(root->right);
+        res = max(res, left + right);
+        return max(left, right) + 1;
+    }
+};
