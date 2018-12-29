e725c854a1cd8c3981458b1534507395380c6171
ahljljj <jliang0718@outlook.com>
2018-12-29 16:48:57 -0700

    cpp, dfs

--- 653. Two Sum IV - Input is a BST.cpp
+++ 653. Two Sum IV - Input is a BST.cpp
@@ -64,3 +64,30 @@ public:
         inorder(root->right, nums);
     }
 };
+
+// cpp, dfs
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> s;
+        return dfs(root, k, s);
+
+    }
+
+    bool dfs(TreeNode* root, int k, unordered_set<int> &s){
+        if (!root) return false;
+        if (s.find(k - root->val) != s.end()) return true;
+        s.insert(root->val);
+        return dfs(root->left, k, s) || dfs(root->right, k, s);
+    }
+};
