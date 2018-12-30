5e1c421763f0846acbc79e576f45d0c0bfa12931
ahljljj <jliang0718@outlook.com>
2018-12-29 17:15:15 -0700

    cpp, dp

--- 121. maxProfit.py
+++ 121. maxProfit.py
@@ -74,4 +74,22 @@ class Solution:
         return maxprofit
 
 
+# cpp, dp
+'''
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        int res = 0, prevMin = prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            res = max(res, prices[i] - prevMin);
+            prevMin = min(prices[i], prevMin);
+        }
+        return res;
+        
+    }
+};
+
+'''
+
 
