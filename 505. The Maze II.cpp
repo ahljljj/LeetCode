492d21d1eefb4dae8c20dbc1977de6412703ee33
ahljljj <jliang0718@outlook.com>
2019-01-24 15:15:16 -0700

    cpp, modified bfs AC

--- 505. The Maze II.cpp
+++ 505. The Maze II.cpp
@@ -87,3 +87,37 @@ public:
         return res < INT_MAX ?res : -1;
     }
 };
+
+// cpp, modified bfs AC
+
+class Solution {
+public:
+    int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        int m = maze.size(), n = maze[0].size();
+        queue<vector<int>> q; vector<vector<bool>> visited(m, vector<bool>(n, false));
+        q.push({start[0], start[1]});
+        int d[m][n];
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                d[i][j] = INT_MAX;
+        d[start[0]][start[1]] = 0;
+        int res = INT_MAX;
+        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!q.empty()){
+            vector<int> curr = q.front();
+            q.pop();
+            if (curr == destination) res = d[curr[0]][curr[1]];
+            for (auto dir: dirs){
+                int i = curr[0] + dir[0], j = curr[1] + dir[1], count = 0;
+                while (i >= 0 && i < m && j >= 0 && j < n && maze[i][j] == 0){
+                    i += dir[0]; j += dir[1]; ++count;
+                }
+                if (d[curr[0]][curr[1]] + count < d[i - dir[0]][j - dir[1]]){
+                    d[i - dir[0]][j - dir[1]] = d[curr[0]][curr[1]] + count;
+                    q.push({i - dir[0], j - dir[1]});
+                }
+            }
+        }
+        return res < INT_MAX ?res : -1;
+    }
+};
