efd5611f6efdc0f1b874ea088be6f3fbb65eee09
ahljljj <jliang0718@outlook.com>
2019-01-31 20:10:43 -0700

    cpp

--- 11. ContainerWithMostWater.py
+++ 11. ContainerWithMostWater.py
@@ -49,3 +49,21 @@ class Solution(object):
             else:
                 r-=1
         return area
+
+'''
+// cpp 
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = INT_MIN, l = 0, r = height.size() - 1;
+        while (l < r){
+            area = max(area, (r - l) * min(height[l], height[r]));
+            if (height[l] < height[r]) ++l;
+            else --r;
+        }
+        return area;
+        
+    }
+};
+'''
