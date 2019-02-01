87796083a9bc87fa0f7e4bba31c43d31a7271e5a
ahljljj <jliang0718@outlook.com>
2019-02-01 09:52:36 -0700

    cpp, rewrite

--- 21. mergeTwoLists.py
+++ 21. mergeTwoLists.py
@@ -41,3 +41,48 @@ class Solution:
             l2=l2.next
             current=current.next
         return new.next
+
+'''
+
+// cpp, rewrite
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* head = dummy;
+        while (l1 && l2){
+            ListNode* curr = new ListNode(-1);
+            if (l1->val < l2->val) {
+                curr->val = l1->val;
+                l1 = l1->next;
+            } else{
+                curr->val = l2->val;
+                l2 = l2->next;
+            }
+            dummy->next = curr;
+            dummy = dummy->next;
+        }
+        while (l1){
+            dummy->next = l1;
+            l1 = l1->next;
+            dummy = dummy->next;
+        }
+        while (l2){
+            dummy->next = l2;
+            l2 = l2->next;
+            dummy = dummy->next;
+        }
+        return head->next;
+        
+    }
+};
+'''
