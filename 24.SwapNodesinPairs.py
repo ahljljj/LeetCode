e2453574b93e093c3b821505fc1a557cb0b421ee
Jinjin Liang <jliang0718@outlook.com>
2019-02-01 17:30:44 -0700

    cpp, recursion

--- 24.SwapNodesinPairs.py
+++ 24.SwapNodesinPairs.py
@@ -47,3 +47,29 @@ class Solution(object):
             curr = curr.next
             prev = tmp.next
         return dummy.next
+
+
+'''
+
+// cpp, recursion
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* n= head->next;
+        head->next = swapPairs(head->next->next);
+        n->next = head;
+        return n;
+        
+    }
+};
+'''
