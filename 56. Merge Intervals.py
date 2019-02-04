ca475eecebae098b7ac160718b35ca8f0e48360d
ahljljj <jliang0718@outlook.com>
2019-02-04 00:13:59 -0700

    c++, rewrite

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -47,7 +47,43 @@ class Solution:
                 tmp = itv
         return res
 
+# c++, rewrite
 
+'''
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        if (intervals.empty()) return intervals;
+        sort(intervals.begin(), intervals.end(), [](Interval& a, Interval& b){ return a.start < b.start; });
+        vector<Interval> res;
+        int l = intervals[0].start, r = intervals[0].end;
+        for (int i = 1; i < intervals.size(); ++i){
+            if (intervals[i].start > r){
+                res.push_back(Interval(l, r));
+                l = intervals[i].start; r = intervals[i].end;
+            }
+            else{
+                r = max(r, intervals[i].end);
+            }
+            
+        }
+        res.push_back(Interval(l, r));
+        return res;
+        
+    }
+    
+};
+
+'''
 
 
 
