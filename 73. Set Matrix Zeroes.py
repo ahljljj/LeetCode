94ef0d3a30861ba172173106be06422fbc6ccd6d
Jinjin Liang <jliang0718@outlook.com>
2019-02-05 10:14:48 -0700

    cpp, brute force

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -81,3 +81,27 @@ class Solution:
         if idx_col:
             for i in range(n):
                 matrix[i][0] = 0
+
+# cpp, brute force
+
+'''
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int> rows, cols;
+        for (int i = 0; i < matrix.size(); ++i){
+            for (int j = 0; j < matrix[0].size(); ++j){
+                if (matrix[i][j] == 0){
+                    rows.insert(i); cols.insert(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.size(); ++i){
+            for (int j = 0; j < matrix[0].size(); ++j){
+                if (rows.find(i) != rows.end() || cols.find(j) != cols.end()) matrix[i][j] = 0;
+            }
+        }
+        
+    }
+};
+'''
