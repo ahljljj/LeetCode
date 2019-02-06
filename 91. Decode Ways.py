e1ba08a8c2e5044bbbe384e0413770f43c9473ae
ahljljj <jliang0718@outlook.com>
2019-02-05 18:49:02 -0700

    cpp, dynamic programming, rewrite

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -80,3 +80,22 @@ class Solution:
                 if 10 <= int(s[i - 1] + s[i]) <= 26:
                     counts[i] += counts[i - 2]
         return counts[n - 1]
+
+# cpp, dynamic programming, rewrite
+
+'''
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0') return 0;
+        vector<int> dp(s.size() + 1);
+        dp[0] = 1; dp[1] = 1;
+        for (int i = 2; i <= s.size(); ++i){
+            if (s[i - 1] != '0') dp[i] += dp[i - 1];
+            if (s[i - 2] != '0' && stoi(s.substr(i - 2, 2))  <= 26) dp[i] += dp[i - 2];
+        }
+        return dp.back();
+        
+    }
+};
+'''
