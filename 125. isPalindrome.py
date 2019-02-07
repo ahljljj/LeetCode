856ad5489d78e9170676e4232fc1d262973875b5
Jinjin Liang <jliang0718@outlook.com>
2019-02-07 10:52:03 -0700

    python, rewrite 60 ms

--- 125. isPalindrome.py
+++ 125. isPalindrome.py
@@ -36,3 +36,31 @@ class Solution:
             return True
         else:
             return False
+
+# python, rewrite 60 ms
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not ('a' <= s[l] <= 'z' or '0' <= s[l] <= '9'):
+                l += 1
+                continue
+            if not ('a' <= s[r] <= 'z' or '0' <= s[r] <= '9'):
+                r -= 1
+                continue
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+
+
+
+
