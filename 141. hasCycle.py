5ccaf8306e4acca5b1195c831bc9d37a1ab296fa
ahljljj <jliang0718@outlook.com>
2019-02-09 12:10:27 -0700

    cpp, two pointers, rewrite

--- 141. hasCycle.py
+++ 141. hasCycle.py
@@ -31,3 +31,33 @@ class Solution(object):
                 J.add(current)
             current=current.next
         return False
+
+
+# cpp, two pointers, rewrite
+
+'''
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) return false;
+        ListNode *slow = head, *fast = head->next;
+        while (slow != fast){
+            if (fast == nullptr || fast->next == nullptr) return false;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+        
+    }
+};
+'''
