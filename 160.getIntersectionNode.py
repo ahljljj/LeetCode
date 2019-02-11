4ac2f025c47fba7b07dff022f34295c6b5cd4728
Jinjin Liang <jliang0718@outlook.com>
2019-02-11 13:48:02 -0700

    cpp, constant space

--- 160.getIntersectionNode.py
+++ 160.getIntersectionNode.py
@@ -92,3 +92,32 @@ public:
 };
 
 '''
+
+#cpp, constant space
+
+'''
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (headA == nullptr || headB == nullptr) return nullptr;
+        ListNode *p1 = headA, *p2 = headB;
+        while (p1 != p2){
+            p1 = p1->next;
+            p2 = p2->next;
+            if (p1 == p2) return p1;
+            if (p1 == nullptr) p1 = headB;
+            if (p2 == nullptr) p2 = headA;
+        }
+        return p1;
+        
+    }
+};
+'''
