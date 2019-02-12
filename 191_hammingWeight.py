be53f65cb619f5b37443c277cec266be99400536
Jinjin Liang <jliang0718@outlook.com>
2019-02-12 13:02:12 -0700

    cpp, rewrite

--- 191_hammingWeight.py
+++ 191_hammingWeight.py
@@ -33,3 +33,20 @@ class Solution(object):
             if int(s):
                 count += 1
         return count
+
+#cpp, rewrite
+
+'''
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n > 0){
+            res += n & 1;
+            n >>= 1;
+        }
+        return res;
+        
+    }
+};
+'''
