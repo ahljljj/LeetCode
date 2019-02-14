a59e79f88b30a4267c2ce6100d5aad150c48973f
Jinjin Liang <jliang0718@outlook.com>
2019-02-14 14:30:52 -0700

    cpp, recursive solution

--- 206_reverseList.py
+++ 206_reverseList.py
@@ -90,3 +90,27 @@ public:
 };
 '''
 
+# cpp, recursive solution
+
+'''
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode *p = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return p;
+        
+    }
+};
+'''
+
