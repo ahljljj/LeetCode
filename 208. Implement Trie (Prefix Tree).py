6fa31cf07bc2888362b4652ab214970088641e64
Jinjin Liang <jliang0718@outlook.com>
2019-02-20 13:42:30 -0700

    cpp, rewrite

--- 208. Implement Trie (Prefix Tree).py
+++ 208. Implement Trie (Prefix Tree).py
@@ -81,3 +81,93 @@ class Trie(object):
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cpp, rewrite
+
+'''
+struct TrieNode{
+    unordered_map<char, TrieNode*> kids;
+    bool endOfWord;
+    TrieNode(bool b = false){
+        endOfWord = b;
+    }
+};
+
+
+
+class Trie {
+    TrieNode *root;
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode();
+    }
+    ~Trie(){
+        destroy(root);
+    }
+    void destroy(TrieNode *node){
+        for (auto &p: node->kids){
+            destroy(p.second);
+        }
+        delete node;
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode *curr = root;
+        for (char &s: word){
+            if (curr->kids.find(s) == curr->kids.end()){
+                curr->kids[s] = new TrieNode();
+            }
+            curr = curr->kids[s];
+        }
+        curr->endOfWord = true;
+        
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode *curr = root;
+        for (char &s: word){
+            if (curr->kids.find(s) == curr->kids.end()) return false;
+            curr = curr->kids[s];
+        }
+        return curr->endOfWord;
+        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode *curr = root;
+        for (char &s: prefix){
+            if (curr->kids.find(s) == curr->kids.end()) return false;
+            curr = curr->kids[s];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * bool param_2 = obj.search(word);
+ * bool param_3 = obj.startsWith(prefix);
+ */
+'''
