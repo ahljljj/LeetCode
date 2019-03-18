03df827103a9fddbdae70863b742a2970c0f6635
Jinjin Liang <jliang0718@outlook.com>
2019-03-18 15:38:47 -0700

    cpp, rewrite

--- 341. Flatten Nested List Iterator.py
+++ 341. Flatten Nested List Iterator.py
@@ -161,3 +161,62 @@ class NestedIterator(object):
 # i, v = NestedIterator(nestedList), []
 # while i.hasNext(): v.append(i.next())
 '''
+
+
+
+
+
+# cpp, rewrite
+
+'''
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class NestedIterator {
+    stack<NestedInteger> stk;
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        for(auto it = nestedList.rbegin(); it != nestedList.rend(); ++it) stk.push(*it);
+        
+    }
+
+    int next() {
+        auto tp = stk.top();
+        stk.pop();
+        return tp.getInteger();
+        
+    }
+
+    bool hasNext() {
+        while (!stk.empty()){
+            auto curr = stk.top();
+            if (curr.isInteger()) return true;
+            stk.pop();
+            vector<NestedInteger> v = curr.getList();
+            for (auto it = v.rbegin(); it != v.rend(); ++it) stk.push(*it);           
+        }
+        return false;        
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+'''
