7d48523adb1b0fc284b04ca96a6f3515be042f80
Jinjin Liang <jliang0718@outlook.com>
2019-03-21 14:18:00 -0700

    cpp, rewrite

--- 454. 4Sum II.py
+++ 454. 4Sum II.py
@@ -59,6 +59,27 @@ class Solution:
                     res += sumAB[-tmp]
         return res
 
+
+# cpp, rewrite
+
+'''
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int,int> mAB;
+        for (int i = 0; i < A.size(); ++i)
+            for (int j = 0; j < B.size(); ++j)
+                ++mAB[A[i] + B[j]];
+        int res = 0;
+        for (int i = 0; i < C.size(); ++i)
+            for (int j = 0; j < D.size(); ++j)
+                res += mAB[- (C[i] + D[j])];
+        return res;
+        
+    }
+};
+'''
+
 # binary search: TLE
 
 class Solution:
