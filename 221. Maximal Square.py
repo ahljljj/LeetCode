5493c634f87bba56332aa8e1e08264ae740ee531
Jinjin Liang <jliang0718@outlook.com>
2019-04-02 15:50:57 -0700

    cpp, dynamic programming, rewrite

--- 221. Maximal Square.py
+++ 221. Maximal Square.py
@@ -53,3 +53,28 @@ class Solution:
                     maxlen = max(dp[i][j], maxlen)
         return maxlen ** 2
 
+# cpp, dynamic programming, rewrite
+
+'''
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        int res = 0;
+        for (int i = 1; i <= n; ++i){
+            for (int j = 1; j <= m; ++j){
+                if (matrix[i - 1][j - 1] == '1'){
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                res = max(res, dp[i][j]);
+            }
+        }
+        return res * res;
+        
+        
+    }
+};
+'''
+
