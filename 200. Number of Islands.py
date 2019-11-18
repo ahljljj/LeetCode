0f9b37790d72ef40d6ad117f7cfa53ddf8fcf39e
ahljljj <jliang0718@outlook.com>
2019-11-17 23:22:40 -0500

    Update 200. Number of Islands.py

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -93,3 +93,34 @@ public:
     }
 };
 '''
+
+'''
+11/17/2019
+
+Runtime: 160 ms, faster than 60.09% of Python3 online submissions for Number of Islands.
+Memory Usage: 13.9 MB, less than 61.54% of Python3 online submissions for Number of Islands.
+'''
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if len(grid) == 0:
+            return 0
+        res = 0
+        n, m = len(grid), len(grid[0])
+        visited = [[False] * m for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if not visited[i][j] and grid[i][j] == '1':
+                    self.dfs(grid, visited, n, m, i, j)
+                    res += 1
+        return res
+
+    def dfs(self, grid, visited, n, m, i, j):
+        if i < 0 or j < 0 or i >= n or j >= m or visited[i][j] or grid[i][j] != '1':
+            return
+        visited[i][j] = True
+        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        for (ii, jj) in dirs:
+            ni, nj = i + ii, j + jj
+            self.dfs(grid, visited, n, m, ni, nj)
+
