e59bb13cdd17dda7bd756423fbb0d80069b45136
ahljljj <jliang0718@outlook.com>
2019-11-27 22:30:02 -0500

    Update 90. Subsets II.py

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -41,3 +41,35 @@ class Solution:
             if tmp not in res: res.append(tmp[:])
             self.helper(res, tmp, nums, i + 1)
             tmp.pop()
+
+'''
+2019/11/27, c++, backtracking, not that good
+
+Runtime: 8 ms, faster than 83.30% of C++ online submissions for Subsets II.
+Memory Usage: 10.1 MB, less than 31.82% of C++ online submissions for Subsets II.
+
+
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> res;
+        vector<int> tmp;
+        backtracking(nums, res, tmp, 0);
+        vector<vector<int>> target;
+        for (auto &node: res) target.push_back(node);
+        return target;
+        
+    }
+    
+    void backtracking(vector<int> &nums, set<vector<int>> &res, vector<int> tmp, int start){
+        res.insert(tmp);
+        for (int i = start; i < nums.size(); ++i){
+            tmp.push_back(nums[i]);
+            backtracking(nums, res, tmp, i + 1);
+            tmp.pop_back();
+        }
+    }
+};
+'''
