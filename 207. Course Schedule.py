a603a5f6a4be7862bb783e5fd4d360924616e4be
ahljljj <jliang0718@outlook.com>
2020-04-01 20:38:19 -0400

    Update 207. Course Schedule.py

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -91,3 +91,43 @@ public:
     }
 };
 '''
+
+
+# 2020/04/01, BFS on graph
+
+'''
+Runtime: 88 ms, faster than 98.94% of Python3 online submissions for Course Schedule.
+Memory Usage: 15.4 MB, less than 65.31% of Python3 online submissions for Course Schedule.
+'''
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        in_degree = self.get_in_degree(numCourses, prerequisites)
+        neighbors = self.get_neighbors(numCourses, prerequisites)
+        start = [x for x in range(numCourses) if in_degree[x] == 0]
+        res = 0
+        q = collections.deque(start)
+        while q:
+            front = q.popleft()
+            res += 1
+            for nei in neighbors[front]:
+                in_degree[nei] -= 1
+                if in_degree[nei] <= 0: q.append(nei)
+        return res == numCourses
+
+    def get_in_degree(self, n, graph):
+        # course #: 0, 1,..., n - 1
+        # if (course, pre_course) in graph,
+        # then add 1 to the in degree of this course
+        m = [0] * n
+        for (course, _) in graph:
+            m[course] += 1
+        return m
+
+    def get_neighbors(self, n, graph):
+        # course #: 0, 1, ..., n - 1
+        # for each course, get all down stream courses
+        m = {x: [] for x in range(n)}
+        for (course, pre_course) in graph:
+            m[pre_course].append(course)
+        return m
