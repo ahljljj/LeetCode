ce8fc63226ca08be6697e6b2250dfd08d7c48873
ahljljj <jliang0718@outlook.com>
2020-04-07 16:25:44 -0400

    Update 98. Validate Binary Search Tree.py

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -107,3 +107,39 @@ public:
 };
 
 '''
+
+
+# 2020/04/04, divide and conquer
+
+'''
+Runtime: 52 ms, faster than 11.26% of Python3 online submissions for Validate Binary Search Tree.
+Memory Usage: 16.4 MB, less than 9.20% of Python3 online submissions for Validate Binary Search Tree.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        _, _, res = self.dfs(root)
+        return res
+
+    def dfs(self, root):
+        # stopping condition is crucial
+        # max cannot exceed the current value and min cannot less the current val
+        if not root: return (float("inf"), -float("inf"), True)
+        left_min, left_max, left_valid = self.dfs(root.left)
+        right_min, right_max, right_valid = self.dfs(root.right)
+        # determine if the current subtree is valid according to the results
+        # from left and right subtree
+        if not left_valid or not right_valid or left_max >= root.val or right_min <= root.val:
+            return (float("inf"), -float("inf"), False)
+        # update the min and max value
+        curr_min = min([left_min, right_min, root.val])
+        curr_max = max([left_max, right_max, root.val])
+        return (curr_min, curr_max, True)
