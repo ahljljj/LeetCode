b36e0694d200b2fb28c5e31246b8ff52264143ab
ahljljj <jliang0718@outlook.com>
2020-04-08 14:39:28 -0400

    Update 230. Kth Smallest Element in a BST.py

--- 230. Kth Smallest Element in a BST.py
+++ 230. Kth Smallest Element in a BST.py
@@ -96,3 +96,77 @@ class Solution:
             self.res = root.val
             return
         self.helper(root.right)
+
+# 2020/04/08, divide and conquer
+
+'''
+Runtime: 56 ms, faster than 34.92% of Python3 online submissions for Kth Smallest Element in a BST.
+Memory Usage: 17.9 MB, less than 5.45% of Python3 online submissions for Kth Smallest Element in a BST.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        _, node = self.div_conq(root, k)
+        return node.val
+
+    def div_conq(self, root, k):
+        if not root: return (0, None)
+        left_size, left_subtree = self.div_conq(root.left, k)
+        right_size, right_subtree = self.div_conq(root.right, k - left_size - 1)
+        if left_size + 1 == k: return (k, root)
+        if left_subtree: return (k, left_subtree)
+        if right_subtree: return (k, right_subtree)
+        return (left_size + 1 + right_size, None)
+
+# slight modification
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        _, node = self.div_conq(root, k)
+        return node.val
+
+    def div_conq(self, root, k):
+        if not root: return (0, None)
+        left_size, left_subtree = self.div_conq(root.left, k)
+        right_size, right_subtree = self.div_conq(root.right, k - left_size - 1)
+        if left_size + 1 == k: return (k, root)
+        return (left_size + 1 + right_size, left_subtree or right_subtree)
+
+# for fun
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        res = []
+        self.dfs(root, res, [k])
+        return res[0]
+
+    def dfs(self, root, res, k):
+        if not root: return None
+        self.dfs(root.left, res, k)
+        k[0] -= 1
+        if k[0] == 0:
+            res.append(root.val)
+            return
+        self.dfs(root.right, res, k)
