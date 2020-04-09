655e4b6ca738190768de3c825e226bf8c4fea61d
ahljljj <jliang0718@outlook.com>
2020-04-09 15:42:24 -0400

    Update 701. Insert into a Binary Search Tree.py

--- 701. Insert into a Binary Search Tree.py
+++ 701. Insert into a Binary Search Tree.py
@@ -66,3 +66,14 @@ class Solution:
                 root = root.right
         return dummy
 
+# recursion
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root: return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
