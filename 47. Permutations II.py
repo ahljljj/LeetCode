60ef141e3c2af6f30d476f50794770f9c1d1491f
ahljljj <jliang0718@outlook.com>
2020-04-19 18:20:23 -0400

    Update 47. Permutations II.py

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -44,3 +44,32 @@ class Solution:
 
         dfs(nums, [])
         return res
+
+# 2020/04/19, permutations + reduce duplicates
+
+'''
+Runtime: 60 ms, faster than 50.82% of Python3 online submissions for Permutations II.
+Memory Usage: 14.1 MB, less than 6.67% of Python3 online submissions for Permutations II.
+'''
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        visited = [False] * len(nums)
+        nums.sort()
+        self.dfs(nums, res, [], visited)
+        return res
+
+    def dfs(self, nums, result, permutation, visited):
+        if len(permutation) == len(nums):
+            result.append(permutation[:])
+        for i in range(len(nums)):
+            if visited[i]: continue
+            if i > 0 and nums[i] == nums[i - 1] and not visited[i - 1]:
+                continue
+            visited[i] = True
+            permutation.append(nums[i])
+            self.dfs(nums, result, permutation, visited)
+            permutation.pop()
+            visited[i] = False
