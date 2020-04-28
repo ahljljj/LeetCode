c5a2a1c1db27a7bcc9650818776febea2a90c6ac
ahljljj <jliang0718@outlook.com>
2020-04-28 19:36:08 -0400

    Update 380. Insert Delete GetRandom O(1).py

--- 380. Insert Delete GetRandom O(1).py
+++ 380. Insert Delete GetRandom O(1).py
@@ -144,3 +144,57 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+
+# 2020/04/28, too hard, hashtable + array
+
+'''
+Runtime: 96 ms, faster than 89.94% of Python3 online submissions for Insert Delete GetRandom O(1).
+Memory Usage: 18 MB, less than 12.50% of Python3 online submissions for Insert Delete GetRandom O(1).
+'''
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.key_to_index = {}
+        self.nums = []
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.key_to_index:
+            return False
+        self.nums.append(val)
+        self.key_to_index[val] = len(self.nums) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.key_to_index:
+            return False
+        i = self.key_to_index[val]
+        tail = self.nums.pop()
+        if i < len(self.nums):
+            self.nums[i] = tail
+            self.key_to_index[tail] = i
+        del self.key_to_index[val]
+        return True
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
