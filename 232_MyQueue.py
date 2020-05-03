c88050a0438253d3d3969b766e977720c472d7d8
ahljljj <jliang0718@outlook.com>
2020-05-03 13:13:21 -0400

    Update 232_MyQueue.py

--- 232_MyQueue.py
+++ 232_MyQueue.py
@@ -70,3 +70,49 @@ class MyQueue(object):
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+
+
+# 2020/05/03, two stacks
+
+'''
+Runtime: 28 ms, faster than 64.14% of Python3 online submissions for Implement Queue using Stacks.
+Memory Usage: 13.9 MB, less than 10.00% of Python3 online submissions for Implement Queue using Stacks.
+'''
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.s1 and not self.s2
