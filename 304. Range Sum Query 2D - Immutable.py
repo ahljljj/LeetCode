deffcfc6223e2f12eebf20a1c83feb12e6ca8267
ahljljj <jliang0718@outlook.com>
2020-05-06 23:19:23 -0400

    Update 304. Range Sum Query 2D - Immutable.py

--- 304. Range Sum Query 2D - Immutable.py
+++ 304. Range Sum Query 2D - Immutable.py
@@ -58,3 +58,29 @@ private:
  * int param_1 = obj.sumRegion(row1,col1,row2,col2);
  */
 '''
+
+
+# 2020/05/06, dynamic programming
+
+'''
+Runtime: 116 ms, faster than 63.76% of Python3 online submissions for Range Sum Query 2D - Immutable.
+Memory Usage: 16.9 MB, less than 16.67% of Python3 online submissions for Range Sum Query 2D - Immutable.
+'''
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        if not matrix or not matrix[0]:
+            self.dp = [[0]]
+            return
+        n, m = len(matrix), len(matrix[0])
+        self.dp = [[0] * (1 + m) for _ in range(1 + n)]
+        for i in range(n):
+            for j in range(m):
+                self.dp[i + 1][j + 1] = self.dp[i + 1][j] + self.dp[i][j + 1] \
+                                        - self.dp[i][j] + matrix[i][j]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.dp[row2 + 1][col2 + 1] \
+               - self.dp[row2 + 1][col1] - self.dp[row1][col2 + 1] \
+               + self.dp[row1][col1]
