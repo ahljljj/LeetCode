c69c0c24b02f72e09d2c057bc79100d4978b287b
ahljljj <jliang0718@outlook.com>
2020-05-12 09:56:03 -0400

    Update 120. Triangle.py

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -39,3 +39,22 @@ class Solution(object):
                     layer[col] = triangle[row][col] + min(tmp[col - 1], tmp[col])
         return min(layer)
 
+# 2020/05/12, dp, space O(n^2)
+'''
+Runtime: 56 ms, faster than 87.60% of Python3 online submissions for Triangle.
+Memory Usage: 14.9 MB, less than 6.67% of Python3 online submissions for Triangle.
+'''
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[0] * (i + 1) for i in range(n)]
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            dp[i][0] = dp[i-1][0] + triangle[i][0]
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i]
+        for i in range(2, n):
+            for j in range(1, i):
+                dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
+        return min(dp[n-1])
+
