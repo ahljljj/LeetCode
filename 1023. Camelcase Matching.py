2044a3a62c80006091309dccd651901ab176f875
ahljljj <jliang0718@outlook.com>
2020-06-06 20:05:14 -0400

    Create 1023. Camelcase Matching.py

---
+++ 1023. Camelcase Matching.py
@@ -0,0 +1,91 @@
+'''
+1023. Camelcase Matching
+
+A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
+
+Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
+
+
+
+Example 1:
+
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB"
+Output: [true,false,true,true,false]
+Explanation:
+"FooBar" can be generated like this "F" + "oo" + "B" + "ar".
+"FootBall" can be generated like this "F" + "oot" + "B" + "all".
+"FrameBuffer" can be generated like this "F" + "rame" + "B" + "uffer".
+Example 2:
+
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBa"
+Output: [true,false,true,false,false]
+Explanation:
+"FooBar" can be generated like this "Fo" + "o" + "Ba" + "r".
+"FootBall" can be generated like this "Fo" + "ot" + "Ba" + "ll".
+Example 3:
+
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBaT"
+Output: [false,true,false,false,false]
+Explanation:
+"FooBarTest" can be generated like this "Fo" + "o" + "Ba" + "r" + "T" + "est".
+
+
+Note:
+
+1 <= queries.length <= 100
+1 <= queries[i].length <= 100
+1 <= pattern.length <= 100
+All strings consists only of lower and upper case English letters.
+Accepted
+16,062
+Submissions
+28,541
+
+'''
+
+
+# 2020/06/06, trie
+
+'''
+Runtime: 40 ms, faster than 13.75% of Python3 online submissions for Camelcase Matching.
+Memory Usage: 13.9 MB, less than 32.64% of Python3 online submissions for Camelcase Matching.
+'''
+
+
+class Solution:
+    def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+        trie = Trie()
+        trie.insert(pattern)
+        ans = []
+        for query in queries:
+            ans.append(trie.find(query))
+        return ans
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_word = True
+
+    def find(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children and c.isupper():
+                return False
+            elif c in node.children:
+                node = node.children[c]
+        return node.is_word
+
