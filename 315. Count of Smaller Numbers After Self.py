e6bf6f943c084240d5241dab5d0dad6d79e801f6
ahljljj <jliang0718@outlook.com>
2020-06-08 14:17:54 -0400

    Update 315. Count of Smaller Numbers After Self.py

--- 315. Count of Smaller Numbers After Self.py
+++ 315. Count of Smaller Numbers After Self.py
@@ -58,3 +58,77 @@ class BITree:
             i -= self.lowbit(i)
         return res
 
+# 2020/06/08, segment tree, not that fast
+
+'''
+Runtime: 396 ms, faster than 12.28% of Python3 online submissions for Count of Smaller Numbers After Self.
+Memory Usage: 19.5 MB, less than 5.77% of Python3 online submissions for Count of Smaller Numbers After Self.
+'''
+
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        if not nums: return []
+        min_val, max_val = min(nums), max(nums)
+        root = SegmentTree.build(min_val, max_val)
+        ans = [None] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            SegmentTree.modify(root, nums[i], 1)
+            ans[i] = SegmentTree.query(root, min_val, nums[i] - 1)
+        return ans
+
+
+class SegmentTree:
+
+    def __init__(self, start, end, count):
+        self.start = start
+        self.end = end
+        self.count = count
+        self.left, self.right = None, None
+
+    @classmethod
+    def build(cls, start, end):
+        if start > end:
+            return
+        root = SegmentTree(start, end, 0)
+        if start == end:
+            return root
+        mid = (start + end) // 2
+        root.left = cls.build(start, mid)
+        root.right = cls.build(mid + 1, end)
+        return root
+
+    @classmethod
+    def modify(cls, root, index, value):
+        if root.start == root.end:
+            root.count += value
+            return
+        mid = (root.start + root.end) // 2
+        if index <= mid:
+            cls.modify(root.left, index, value)
+        else:
+            cls.modify(root.right, index, value)
+        root.count = root.left.count + root.right.count
+        return
+
+    @classmethod
+    def query(cls, root, start, end):
+        if not root: return 0
+        if start <= root.start and root.end <= end:
+            return root.count
+        mid = (root.start + root.end) // 2
+        if end <= mid:
+            return cls.query(root.left, start, end)
+        if start > mid:
+            return cls.query(root.right, start, end)
+        l = cls.query(root.left, start, mid)
+        r = cls.query(root.right, mid + 1, end)
+        return l + r
+
+
+
+
+
+
+
+
