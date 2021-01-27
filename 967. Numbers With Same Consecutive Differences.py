2f77dfbb961a9945a99d3b2363b9cf5385375d5f
ahljljj <jliang0718@outlook.com>
2021-01-27 11:37:22 -0500

    Update 967. Numbers With Same Consecutive Differences.py

--- 967. Numbers With Same Consecutive Differences.py
+++ 967. Numbers With Same Consecutive Differences.py
@@ -32,3 +32,26 @@ class Solution:
         ans = [int(num) for num in q if num[0] != '0']
         return ans
 
+# 2021/01/27
+# 经过优化后的bfs
+# 由低位向高位层序遍历，每次新产生的数都是最低位
+
+
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        q = collections.deque( [str(i) for i in range(10)])
+        length = 1
+        visited = set(q)
+        while length < n:
+            size = len(q)
+            for _ in range(size):
+                front = q.popleft()
+                suffix = [ int(front[-1]) - k ,   int(front[-1]) + k ]
+                for num in suffix:
+                    if 0 <= num < 10 and front + str(num) not in visited:
+                        q.append( front + str(num) )
+                        visited.add( front + str(num) )
+            length += 1
+        ans = [ int(num) for num in q if num[0] != '0' ]
+        return ans
+
