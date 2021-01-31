57e6d4522a967f0b5abea7cd28363b7a75799c5c
ahljljj <jliang0718@outlook.com>
2021-01-31 13:54:48 -0500

    Update 254. Factor Combinations.py

--- 254. Factor Combinations.py
+++ 254. Factor Combinations.py
@@ -130,3 +130,44 @@ public:
     }
 };
 '''
+
+
+# 2021/01/31
+
+# Runtime: 64 ms, faster than 34.07% of Python3 online submissions for Factor Combinations.
+# Memory Usage: 14.2 MB, less than 57.57% of Python3 online submissions for Factor Combinations.
+
+# 全子集标准模板
+# 先将所有的unique factor 找出来
+# 再利用全子集的想法来从unique factor set 中找出合适的子集
+
+
+class Solution:
+    def getFactors(self, n: int) -> List[List[int]]:
+        ans = []
+        factors = self.get_unique_factors(n)
+        factors.sort()
+        self.dfs(factors, n, 0, ans, [])
+        return ans
+
+    def get_unique_factors(self, n):
+        i = 2
+        ans = []
+        while i < n and i * i <= n:
+            if n % i == 0:
+                ans.append(i)
+                if n // i != i: ans.append(n // i)
+            i += 1
+        return ans
+
+    def dfs(self, nums, n, start, ans, subset):
+        if subset:
+            if math.prod(subset) == n:
+                ans.append(subset[:])
+                return
+            if math.prod(subset) > n:
+                return
+        for i in range(start, len(nums)):
+            subset.append(nums[i])
+            self.dfs(nums, n, i, ans, subset)
+            subset.pop()
